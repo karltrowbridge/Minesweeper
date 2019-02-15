e2a3f9506e7924a6f82d37e4cd230c2a503b3830
karltrowbridge <42477004+karltrowbridge@users.noreply.github.com>
2019-02-15 17:38:30 -0600

    Just not sure what to do

--- package/BoardPanel.java
+++ package/BoardPanel.java
@@ -38,6 +38,8 @@ public class BoardPanel extends JPanel
     public void setDisplayCount(int count)
     { displayCount = count; }
 
+    
+    
     private Minesweeper game;
 
     public void update(Minesweeper game)
