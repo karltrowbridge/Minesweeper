5f9bb04993284cd861ecf4e31b80a4dc60774495
karltrowbridge <42477004+karltrowbridge@users.noreply.github.com>
2019-03-21 19:42:40 -0500

    bruh

---
+++ package/NumberThing.java
@@ -0,0 +1,11 @@
+
+/**
+ * Write a description of class NumberThing here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class NumberThing
+{
+    private long num 00000000000000000000
+}
