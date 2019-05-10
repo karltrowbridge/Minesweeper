2a6c67b9e0ddd7c038fafe572d29b33a6b021004
karltrowbridge <42477004+karltrowbridge@users.noreply.github.com>
2019-05-10 17:38:12 -0500

    not much smh

---
+++ package/Earning.java
@@ -0,0 +1,11 @@
+
+/**
+ * Earnig object holds info that was made from one day.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Earning
+{
+    public Earning(int tips, double hours,
+}
