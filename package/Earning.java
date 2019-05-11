8aa2d4226cab7eeff36e833000083d3518cce887
karltrowbridge <42477004+karltrowbridge@users.noreply.github.com>
2019-05-10 21:17:01 -0500

    not much

--- package/Earning.java
+++ package/Earning.java
@@ -1,4 +1,5 @@
 
+import java.text.SimpleDateFormat;
 /**
  * Earnig object holds info that was made from one day.
  *
@@ -7,5 +8,28 @@
  */
 public class Earning
 {
-    public Earning(int tips, double hours,
+    private int tips;
+    private double hours;
+    private SimpleDateFormat date;
+    
+    public Earning(int tips, double hours, SimpleDateFormat date)
+    {
+        this.tips = tips;
+        this.hours = hours;
+        this.date = date;
+    }
+    
+    // get methods
+    public int getTips()
+    {return this.tips;}
+    
+    public double getHours()
+    {return this.hours;}
+    
+    public SimpleDateFormat getDate()
+    {return this.date;}
+    
+    // set methods
+    public void setTips(int newTips)
+    {this.tips = newTips
 }
